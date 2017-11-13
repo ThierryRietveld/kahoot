dbf74f64b2782cfea287f7a8d9155044c9cff93a
magnetronoven <thierry.rietveld0505@gmail.com>
2017-11-13 17:09:43 +0100

    Router gemaakt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,38 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { UserComponent } from './components/user/user.component';
 
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+
+const appRoutes: Routes = [
+  { path: 'user', component: UserComponent },
+  // { path: 'hero/:id',      component: HeroDetailComponent },
+  // {
+  //   path: 'heroes',
+  //   component: HeroListComponent,
+  //   data: { title: 'Heroes List' }
+  // },
+  { path: '',
+    redirectTo: '/user',
+    pathMatch: 'full'
+  },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
+    UserComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    HttpClientModule,
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+ 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
@@ -8,9 +9,21 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+  }
+
+  creatPost(){
+    console.log("banaan");
+    const data = {
+      id: 2,
+      name: 'mandje'
+    }
+
+    this.http.get('http://localhost:4201/banaan').subscribe(data => {
+      console.log(data);
+    });
   }
 
 }
