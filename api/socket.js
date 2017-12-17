2736f08ee4f06fb1e737622fee5d82a3b4e80f2b
magnetronoven <311789@student.mboutrecht.nl>
2017-12-17 16:46:46 +0100

    breh

--- api/socket.js
+++ api/socket.js
@@ -1,33 +1,44 @@
 // Socket.io voor de win!
 
+/* 
+
+    Probleemen:
+
+    * Hij connect altijd 2 keer.
+      Dus er zitten altijd 2 de zelfde personen in het connection array.
+
+*/
+
 var connections = [];
 var users = {};
 var placeholder = [];
 var rooms = {};
 
-io.on('connection', function(socket){
-    
+io.on('connection', function (socket) {
+
     connections.push(socket);
     console.log('User connected');
+
     // Disconnect
-    socket.on('disconnect', function(data){
-        for(var i = 0;i < connections.length; i++){
-            if(connections[i].name == socket.username){
-                connections.splice(i,1);
+    socket.on('disconnect', function (data) {
+        for (var i = 0; i < connections.length; i++) {
+            if (connections[i].name == socket.username) {
+                connections.splice(i, 1);
             }
         }
         delete users[socket.username];
-        connections.splice(connections.indexOf(socket),1);
+        connections.splice(connections.indexOf(socket), 1);
+        console.log("User disconneted");
     });
 
-    socket.on('makeNewGame',function(data){
-        makeNewRoom(data, function(){
+    socket.on('makeNewGame', function (data) {
+        makeNewRoom(data, function () {
             console.log("New room made");
             socket.emit('newGameMade', data);
         });
     });
 
-    function makeNewRoom(data, callback){
+    function makeNewRoom(data, callback) {
         rooms[data.id] = {};
         rooms[data.id]['players'] = {};
         rooms[data.id]['data'] = {};
@@ -38,9 +49,9 @@ io.on('connection', function(socket){
         callback();
     }
 
-    socket.on('connectToRoom', function(data, callback){
-        for(var key in rooms){
-            if(rooms[key]['data']['token'] == data.token){
+    socket.on('connectToRoom', function (data, callback) {
+        for (var key in rooms) {
+            if (rooms[key]['data']['token'] == data.token) {
                 socket.join(key);
                 rooms[key]['players'][data.id] = {};
                 callback(rooms[key]);
@@ -52,7 +63,7 @@ io.on('connection', function(socket){
     // socket.on('new user', function(data, callback){
     //     callback(true,data);
     //     socket.username = data;
-        
+
     //     users[socket.username] = socket;
     //     placeholder.push({name: socket.username, id: users[socket.username].id});
     //     updateUsernames();
--- src/app/services/socket.service.ts
+++ src/app/services/socket.service.ts
@@ -8,44 +8,62 @@ import * as io from "socket.io-client";
 @Injectable()
 export class SocketService {
 
-  connectionOptions =  {
-    "force new connection" : true,
+  connectionOptions = {
+    "force new connection": true,
     "reconnectionAttempts": "Infinity", //avoid having user reconnect manually in order to prevent dead clients after a server restart
-    "timeout" : 10000, //before connect_error and connect_timeout are emitted.
-    "transports" : ["websocket"]
+    "timeout": 10000, //before connect_error and connect_timeout are emitted.
+    "transports": ["websocket"]
   };
 
   public socket;
   private data;
   private dit;
 
-  constructor(public router:Router) {
+  constructor(public router: Router) {
+
     this.socket = io('http://localhost:4201', this.connectionOptions);
-    this.socket.on('newGameMade', function(data){
+
+    this.socket.on('newGameMade', function (data) {
+
       console.log(this);
+      boi();
       // Hier is 'this' niet defined
       // navigateHere();
     });
   }
 
-  makeNewGame(data){
+  makeNewGame(data) {
     this.data = data;
     this.data.id = this.socket.id;
     this.socket.emit('makeNewGame', this.data);
   }
 
-  navigateHere(route){
+  saySomething(text) {
+    console.log(text);
+  }
+
+  navigateHere(route) {
     this.router.navigate([route]);
   }
 
-  connectToRoom(thetoken){
-    this.data = {id: this.socket.id,
-                 token: thetoken};
+  connectToRoom(thetoken) {
 
-    this.socket.emit('connectToRoom', this.data, function(room){
+    this.data = {
+      id: this.socket.id,
+      token: thetoken
+    };
+
+    this.socket.emit('connectToRoom', this.data, function (room) {
       console.log(room);
     });
   }
-  
 
+
+}
+
+
+let socketService = new SocketService();
+
+function boi() {
+  socketService.saySomething('duihdfuihi');
 }
